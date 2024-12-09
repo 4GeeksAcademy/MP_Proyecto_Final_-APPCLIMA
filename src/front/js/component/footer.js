765939469aa2bd8da2b62bd5b253820187d5994d
ElioPeraza <157733983+ElioPeraza@users.noreply.github.com>
2024-12-09 18:14:31 +0000

    commit "update"

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+const Footer = () => {
+    return <footer className="bg-light text-center py-3">© 2024 - App de Clima</footer>;
+};
+
+export default Footer;
+
